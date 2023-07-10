1e5b27ee5c8c5000a537bd4a208ae3fc0283230a
Yesid Gomez <wujibifan.hh@gmail.com>
2023-07-10 18:10:13 -0500

    Inicialización del proyecto
    Creación del componente InitialMessage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import InitialMessage from "./components/InitialMessage";
+
+function App() {
+
+  return (
+    <div className="container">
+        <InitialMessage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InitialMessage.jsx
@@ -0,0 +1,17 @@
+const InitialMessage = () => {
+    return (
+        <div className="initial-msg-container">
+            <div>
+                <img src="../public/fdc-logo.webp" alt="Flores de Colores" />
+                <h1>Flores de Colores</h1>
+            </div>
+            <div>
+                <h2>Registro Taller de arreglos florales</h2>
+                <p>¡Regístrate para nuestro emocionante taller de arreglos florales y descubre el arte de crear hermosos diseños con flores! Completa el formulario a continuación para reservar tu lugar en este evento inspirador y aprender técnicas profesionales de arreglos florales.</p>
+                <button>Continuar</button>
+            </div>
+        </div>
+    )
+}
+
+export default InitialMessage
